9313e470b1b514528cc9d5803f374bc919d2ae3b
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-06-05 17:08:21 +0300

    edit app style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-
 import './App.css'
-
+import DeleteModal from './components/Modal/DeleteModal';
+import Info from './components/Modal/Info';
+import ModalLayout from './components/Modal/ModalLayout';
+import Warning from './components/Modal/Warning';
+import Success from './components/Modal/Success'
+import ErrorModal from './components/Modal/ErrorMondal'
 function App() {
 
   return (
-    <>
-
-    </>
+    <div style={{ display: "flex", flexDirection: "column", gap: "15px", justifyContent: 'center', alignItems: "center", height: "100vh", width: "100vw" }}>
+      <ModalLayout open={true} />
+      <Info />
+      <Success title="Some task has completed!" />
+      <Warning title="This is a warning message" open={true} />
+      <DeleteModal title="Are you sure delete this task?" open={true} />
+      <ErrorModal title="This is an error message" open={true} />
+    </div>
   )
 }
 
--- src/components/Modal/DeleteModal.jsx
+++ src/components/Modal/DeleteModal.jsx
@@ -2,8 +2,8 @@ import ModalLayout from "./ModalLayout"
 import WarningSVG from './assets/ExaminationSVG'
 
 
-const Delete = ({ title, content, open, className }) => {
+const DeleteModal = ({ title, content, open, className }) => {
     return <ModalLayout title={title} deleteFlag={true} icon={<WarningSVG color="#FFC53D" />} className={className} content={content} open={open} />
 }
 
-export default Delete
+export default DeleteModal
