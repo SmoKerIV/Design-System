16f2a2fdafa384897d5cbd3754ac445510589b28
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-06-05 16:57:54 +0300

    finish styling modals

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Warning from './components/Modal/Warning'
 function App() {
 
   return (
-    <div style={{ display: "flex", justifyContent: 'center', alignItems: "center", height: "100vh", width: "100vw" }}>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: "center", height: "100vh", width: "100vw" }}>
       <ModalLayout open={true} />
       <Info />
       <Success title="Some task has completed!" />
