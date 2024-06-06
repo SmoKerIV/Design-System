cc520ad3ac8519995b4be0a7533bdb611e57f0b3
Fatima Alaa Muhammed <fatimaalaa2001xx@gmail.com>
2024-06-06 11:04:45 +0300

    button and app edit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import ModalLayout from './components/Modal/ModalLayout';
 import Warning from './components/Modal/Warning';
 import Success from './components/Modal/Success'
 import ErrorModal from './components/Modal/ErrorMondal'
+import PrimaryButton from './components/buttons/primary-button'
+import LinkButton from './components/buttons/link-button'
+import SecondaryButton from './components/buttons/sec-button'
+import TextButton from './components/buttons/text-button'
+import DashedButton from './components/buttons/dashedbutton'
+import Card from './components/card/card';
+
 function App() {
 
   return (
@@ -14,7 +21,18 @@ function App() {
       <Success title="Some task has completed!" />
       <Warning title="This is a warning message" open={true} />
       <DeleteModal title="Are you sure delete this task?" open={true} />
-      <ErrorModal title="This is an error message" open={true} />
+      <ErrorModal title="This is an error message" open={true} /> 
+      <PrimaryButton title='Primary Button' onClick={onclick}></PrimaryButton>
+      <SecondaryButton title='Secondary Button'onClick={onclick}></SecondaryButton>
+      <LinkButton title='Link Button'onClick={onclick}></LinkButton>
+      <TextButton title='Text Button'onClick={onclick}></TextButton>
+      <DashedButton title='Dashed Button'onClick={onclick}></DashedButton>
+      <Card
+        imageSrc="https://via.placeholder.com/300" 
+        title="Card Title 1" 
+        description="This is a longer description of the card content. Click the button to toggle more or less content." 
+      />
+
     </div>
   )
 }
